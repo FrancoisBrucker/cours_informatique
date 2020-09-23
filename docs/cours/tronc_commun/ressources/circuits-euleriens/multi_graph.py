1e951a33cf9884f56b2233132b69ec6dfcc0cc6a
François Brucker <francois.brucker@centrale-marseille.fr>
2020-09-23 12:08:40 +0200

    eulerian td

--- docs/cours/tronc_commun/ressources/circuits-euleriens/multi_graph.py
+++
@@ -1,9 +0,0 @@
-
-def create_multi_graph(edges):
-    graph = dict()
-    for x, y in edges:
-        graph[x] = []
-        graph[y] = []
-    for x, y in edges:
-        graph[x].append(y)
-    return graph
