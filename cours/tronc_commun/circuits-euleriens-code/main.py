c13ff38c2f3b16ce80c538c838083d840d7a370c
FrancoisBrucker <FrancoisBrucker@users.noreply.github.com>
2021-02-18 14:24:18 +0000

    deploy: c73c61eb321dac5e096e6603ee2b5e0251ac38aa

--- cours/tronc_commun/circuits-euleriens-code/main.py
+++ cours/tronc_commun/circuits-euleriens-code/main.py
@@ -1,19 +1,17 @@
 from multi_graph import is_eulerian, eulerian_circuit
 
-multi_graph = { "00": ["00", "01"],
-                "01": ["10", "11"],
-                "11": ["11", "10"],
-                "10": ["00", "01"],
-               }
+vertices = ["00", "01", "11", "10"]
+edges = [[0, 1], [3, 2], [2, 3], [0, 1]]
 
-print("Le graphe est eulérien :", is_eulerian(multi_graph))
 
-circuit = eulerian_circuit(multi_graph)
+print("Le graphe est eulérien :", is_eulerian(edges))
+
+circuit = eulerian_circuit(edges)
 print("Un circuit eulérien associé est :", circuit)
 
 suite = ""
 
 for mot in circuit:
-    suite += mot[1]
+    suite += vertices[mot][1]
 
 print("Une suite de brujin : ", suite)
