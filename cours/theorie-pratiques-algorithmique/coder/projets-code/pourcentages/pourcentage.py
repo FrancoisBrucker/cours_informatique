0b3edc5abf511ef8d0b460fb2671ab603575dd77
FrancoisBrucker <FrancoisBrucker@users.noreply.github.com>
2022-01-04 11:34:28 +0000

    deploy: 08bcedeff3e56b834b02ac60a704aedbdd14f9b6

---
+++ cours/theorie-pratiques-algorithmique/coder/projets-code/pourcentages/pourcentage.py
@@ -0,0 +1,8 @@
+def pourcent(chaîne_de_caractères):
+    nombre_de_0 = 0
+
+    for c in chaîne_de_caractères:
+        if c == "0":
+            nombre_de_0 += 1
+
+    return nombre_de_0 / len(chaîne_de_caractères)
