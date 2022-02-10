9a8dd7f73c754df1b9b46ffc8ffe85c63194fb74
FrancoisBrucker <FrancoisBrucker@users.noreply.github.com>
2022-02-10 15:05:52 +0000

    deploy: f13d9cebd861c28f812b19512ef064e4f12f7cbf

--- cours/theorie-pratiques-algorithmique/coder/programmation-objet/projet-code/projet-composition-agregation/test_dice.py
+++ cours/theorie-pratiques-algorithmique/coder/programmation-objet/projet-code/projet-composition-agregation/test_dice.py
@@ -1,4 +1,4 @@
-from dice import Dice
+from dice import Dice, TapisVert
 
 
 def test_dice_creation_no_argument():
@@ -6,14 +6,36 @@ def test_dice_creation_no_argument():
     assert dice.get_position() == 1
 
 
-def test_set_position():
+def test_dice_set_position():
     dice = Dice()
     assert dice.get_position() == 1
     dice.set_position(3)
     assert dice.get_position() == 3
 
 
-def test_roll():
+def test_dice_roll():
     dice = Dice()
     dice.roll()
     assert 1 <= dice.get_position() <= 6
+
+
+def test_tapisvert_creation():
+    tapis_vert = TapisVert()
+
+    for d in tapis_vert.get_des():
+        assert d.get_position() == 1
+
+
+def test_tapisvert_modification():
+    tapis_vert = TapisVert()
+    tapis_vert.get_des()[2].set_position(5)
+
+    assert tapis_vert.get_des()[2].get_position() == 5
+
+
+def test_tapisvert_roll():
+    tapis_vert = TapisVert()
+    tapis_vert.roll()
+
+    for d in tapis_vert.get_des():
+        assert 1 <= d.get_position() <= 6
