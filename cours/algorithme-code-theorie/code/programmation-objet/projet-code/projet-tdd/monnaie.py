411e9c58678c5329dab2034fe87053cf40e9464c
FrancoisBrucker <FrancoisBrucker@users.noreply.github.com>
2022-02-21 15:46:41 +0000

    deploy: 3ed0e80d2e1268193950d0e63618c4435e1ddfff

--- cours/algorithme-code-theorie/code/programmation-objet/projet-code/projet-tdd/monnaie.py
+++ cours/algorithme-code-theorie/code/programmation-objet/projet-code/projet-tdd/monnaie.py
@@ -1,6 +1,16 @@
-class Dollar:
+class Monnaie:
     def __init__(self, montant):
         self.montant = montant
 
-    def fois(self, multiplicateur):
-        self.montant *= multiplicateur
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self.montant == other.montant
+
+
+class Dollar(Monnaie):
+    def __mul__(self, multiplicateur):
+        return Dollar(self.montant * multiplicateur)
+
+
+class Franc(Monnaie):
+    def __mul__(self, multiplicateur):
+        return Franc(self.montant * multiplicateur)
