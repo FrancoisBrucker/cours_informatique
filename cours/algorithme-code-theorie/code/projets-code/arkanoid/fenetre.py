9be2238d8c2cf07cda10830c1cc6dca5e332a0fc
FrancoisBrucker <FrancoisBrucker@users.noreply.github.com>
2022-03-17 06:30:49 +0000

    deploy: ad173cf0086e8bdde54a96aa714f46750652ec64

--- cours/algorithme-code-theorie/code/projets-code/arkanoid/fenetre.py
+++ cours/algorithme-code-theorie/code/projets-code/arkanoid/fenetre.py
@@ -1,103 +1,55 @@
 import pyglet
 from pyglet.window import key
 
+from vaisseau import Vaisseau
+
 
 class Fenetre(pyglet.window.Window):
     def __init__(self):
         super().__init__(640, 480, "Arkanoid")
 
-        self.label = pyglet.text.Label(
-            "Hello, world!",
-            x=self.width // 2,
-            y=self.height // 2,
-            anchor_x="center",
-            anchor_y="center",
+        self.score = pyglet.text.Label(
+            "0000",
+            x=630,
+            y=470,
+            anchor_x="right",
+            anchor_y="top",
         )
-        self.dx = 0
-        self.dy = 0
 
-        pyglet.clock.schedule_interval(self.update, 0.5)
+        self.vie = pyglet.text.Label(
+            "2",
+            x=10,
+            y=10,
+            anchor_x="left",
+            anchor_y="bottom",
+        )
+
+        self.sol = pyglet.shapes.Line(0, 50, 640, 50, color=(0xFF, 0xFF, 0xFF))
+
+        self.vaisseau = Vaisseau(50, 640)
+
+        self.direction = 0  # -1 (gauche) ; 0 (immobile) ; 1 (droite)
+        pyglet.clock.schedule_interval(self.update, 1 / 60)
 
     def update(self, dt):
-        if self.dx != 0 or self.dy != 0:
-            self.label = pyglet.text.Label(
-                "Hello, world!",
-                x=max(min(self.label.x + self.dx, self.width), 0),
-                y=max(min(self.label.y + self.dy, self.height), 0),
-                anchor_x="center",
-                anchor_y="center",
-            )
-
-    def on_resize(self, width, height):
-        super().on_resize(width, height)
-
-        self.label = pyglet.text.Label(
-            "Hello, world!",
-            x=width // 2,
-            y=height // 2,
-            anchor_x="center",
-            anchor_y="center",
-        )
-
-    def on_mouse_press(self, x, y, button, modifiers):
-        print("press:", x, y, button)
-
-        if (abs(self.label.x - x) <= self.label.content_width / 2) and (
-            abs(self.label.y - y) <= self.label.content_height / 2
-        ):
-            print("clique dans le label")
-
-    def on_mouse_release(self, x, y, button, modifiers):
-        print("release:", x, y, button)
-
-        if (abs(self.label.x - x) <= self.label.content_width / 2) and (
-            abs(self.label.y - y) <= self.label.content_height / 2
-        ):
-            print("relâcher le bouton dans le label")
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        if (abs(self.label.x - x) <= self.label.content_width / 2) and (
-            abs(self.label.y - y) <= self.label.content_height / 2
-        ):
-            if (abs(self.label.x - (x - dx)) > self.label.content_width / 2) or (
-                abs(self.label.y - (y - dy)) > self.label.content_height / 2
-            ):
-                print("entre dans label")
-        else:
-            if (abs(self.label.x - (x - dx)) <= self.label.content_width / 2) and (
-                abs(self.label.y - (y - dy)) <= self.label.content_height / 2
-            ):
-                print("sort du label")
+        self.vaisseau.bouge(dt, self.direction)
 
     def on_key_press(self, symbol, modifiers):
-        delta = 10
-        if symbol == key.UP:
-            self.dy = delta
-        elif symbol == key.DOWN:
-            self.dy = -delta
-        elif symbol == key.LEFT:
-            self.dx = -delta
+        if symbol == key.LEFT:
+            self.direction += -1
         elif symbol == key.RIGHT:
-            self.dx = +delta
+            self.direction += 1
 
     def on_key_release(self, symbol, modifiers):
-        if symbol == key.UP:
-            self.dy = 0
-        elif symbol == key.DOWN:
-            self.dy = 0
-        elif symbol == key.LEFT:
-            self.dx = 0
+        if symbol == key.LEFT:
+            self.direction -= -1
         elif symbol == key.RIGHT:
-            self.dx = 0
+            self.direction -= 1
 
     def on_draw(self):
-        # print("draw:", self.get_size())
         self.clear()
-        self.label.draw()
+        self.score.draw()
+        self.vie.draw()
+        self.sol.draw()
 
-
-window = HelloWorldWindow()
-print(window.get_size())
-
-pyglet.app.run()
-print("c'est fini !")
+        self.vaisseau.draw()
---
+++ cours/algorithme-code-theorie/code/projets-code/arkanoid/main.py
@@ -0,0 +1,8 @@
+import pyglet
+
+from fenetre import Fenetre
+
+window = Fenetre()
+
+pyglet.app.run()
+print("c'est fini !")
