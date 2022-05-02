b03cae0384de589906afa5dc2794ee2a9b2651f2
FrancoisBrucker <FrancoisBrucker@users.noreply.github.com>
2022-05-02 07:12:21 +0000

    deploy: c0b44b61cd280829784a4931760c40dbb90b8616

--- cours/algorithme-code-theorie/algorithme/etude-recherche-sous-chaines.py
+++ cours/algorithme-code-theorie/algorithme/etude-recherche-sous-chaines.py
@@ -49,10 +49,51 @@ def suite_algorithme_BMH(a, b):
 
 # print(suite_algorithme_BMH("aaaaaaab", "abc"))
 
+def algo_naif_construction_t(b):
+    T_b = []
+
+    for j in range(1, len(b)):
+        T_b.append(0)
+        chaîne = b[1:j]
+        for k in range(len(chaîne)):
+            if b[:k] == chaîne[-k:]:
+                T_b[-1] = k
+
+    return T_b
+
+# print(algo_naif_construction_t("ACGAGACGACT"))
+
+
+
+def cree_tableau(b):
+    T_b = [0]
+
+    j = len(T_b) + 1
+    c = b[j-1]
+
+    while len(T_b) < len(b) - 1:
+        k = T_b[j-2]
+
+        if c == b[k]:
+            T_b.append(k + 1)
+                        
+            j = len(T_b) + 1
+            c = b[j-1]
+        elif k <= 1:
+                T_b.append(0)
+
+                j = len(T_b) + 1
+                c = b[j-1]
+        else:
+            j = k + 1
+
+    return T_b
+
+print(cree_tableau("ACGAGACGACT"))
 
 def sous_chaine_KMP(a, b):
     # Tb = cree_tableau(b)
-    Tb = [1, 2, 2, 2, 5]
+    Tb = [0, 0, 1, 2, 0]
 
     i = 0
     j = 0
@@ -68,10 +109,10 @@ def sous_chaine_KMP(a, b):
             if j == 0:
                 i += 1
             else:
-                l = Tb[j - 1]
+                l = j - Tb[j - 1]
                 i += l
                 j -= l
     return False
 
 
-print(sous_chaine_KMP("ATATATATATCGGGAA", "ATATCG"))
+# print(sous_chaine_KMP("ATATATATATCGGGAA", "ATATCG"))
